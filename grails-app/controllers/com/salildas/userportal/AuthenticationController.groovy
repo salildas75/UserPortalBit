346e11ca9c318cc3db7777a684a246d23b067574
Salil <salildas75@gmail.com>
2019-07-26 00:12:25 +0600

    This is a grails User Portal app

---
+++ grails-app/controllers/com/salildas/userportal/AuthenticationController.groovy
@@ -0,0 +1,27 @@
+package com.salildas.userportal
+
+class AuthenticationController {
+
+    AuthenticationService authenticationService
+
+    def login() {
+        if (authenticationService.isAuthenticated()) {
+            redirect(controller: "dashboard", action: "index")
+        }
+    }
+
+
+    def doLogin() {
+        if (authenticationService.doLogin(params.email, params.password)) {
+            redirect(controller: "dashboard", action: "index")
+        } else {
+            redirect(controller: "authentication", action: "login")
+        }
+    }
+
+
+    def logout() {
+        session.invalidate()
+        redirect(controller: "authentication", action: "login")
+    }
+}
